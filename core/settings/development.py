a1b6018306e5eb3107b94701be16733e64d0699f
taajik <75927372+taajik@users.noreply.github.com>
2026-01-04 19:27:27 +0330

    edit environment variables for postgres

--- core/settings/development.py
+++ core/settings/development.py
@@ -28,10 +28,10 @@ TIME_ZONE = os.environ.get('DJANGO_TIME_ZONE')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
