8eaf970ed9cb5174b10054906f4ec9786a66cdc6
taajik <75927372+taajik@users.noreply.github.com>
2026-05-16 00:12:32 +0330

    send jwt tokens after signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from common.permissions import IsOwnUser
 from .serializers import (
@@ -18,6 +20,20 @@ class UserRegisterAPI(generics.CreateAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSignupSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "user": UserDetailSerializer(user).data,
+            "tokens": {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+        }, status=status.HTTP_201_CREATED)
 
 
 class UserProfileAPI(generics.RetrieveUpdateAPIView):
